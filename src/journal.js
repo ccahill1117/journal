81e9387f1316daf6db0992c89c42eb46ec57e2f2
Christopher Cahill and Devin Sweeting <devin@>
2019-01-07 09:29:51 -0800

    append title and body to blog posts

--- src/journal.js
+++ src/journal.js
@@ -1,3 +1,7 @@
-export function journal() {
+export function Entry(title, content) {
+    this.title = title,
+    this.content = content;
+  };
 
-}
+  var vowelArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "y", "Y"];
+  var consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Z", "q", "Q"];
--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
-import { journal } from './journal';
+import { Entry } from './journal';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-$(document).ready(function() {
-  $('#').submit(function(event) {
-    event.preventDefault();
+// $(document).ready(function() {
+//     event.preventDefault();
+//   const title = $('#blogTitleInput').val();
+//   const content = $('#blogContentInput').val();
+//   var entry = new Entry(title, content);
+//   debugger
+//
+//   });
+// });
+
+$('#postButton').click(function() {
+ const title = $('#blogTitleInput').val();
+ const content = $('#blogContentInput').val();
+ let newEntry = new Entry(title, content);
+ $('.blog-posts').append('<div class="well"<h2>' + newEntry.title + '</h2><p>' + newEntry.content + '</p></div>');
 
 
-  });
+  $('#flashMessage')
+  .slideDown(500)
+  .delay(3000)
+  .slideUp(500);
 });
