bf1c66df297065a27e76cf5371f68b4abccf73da
Christopher Cahill and Devin Sweeting <devin@>
2019-01-07 09:50:29 -0800

    returning id for journal entries

--- src/journal.js
+++ src/journal.js
@@ -1,7 +1,23 @@
 export function Entry(title, content) {
     this.title = title,
     this.content = content;
-  };
+  }
 
   var vowelArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "y", "Y"];
   var consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Z", "q", "Q"];
+
+
+export function Journal() {
+  this.entries = [];
+  this.currentId = 0;
+}
+
+Journal.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Journal.prototype.addEntry = function(entry) {
+  entry.id = this.assignId();
+  this.entries.push(entry);
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import { Entry } from './journal';
+import { Entry, Journal } from './journal';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+var journal = new Journal();
+var entryTest = new Entry("title","body");
+journal.addEntry(entryTest);
+
 
 // $(document).ready(function() {
 //     event.preventDefault();
@@ -18,6 +23,8 @@ $('#postButton').click(function() {
  const title = $('#blogTitleInput').val();
  const content = $('#blogContentInput').val();
  let newEntry = new Entry(title, content);
+ journal.addEntry(newEntry);
+ console.log(journal.entries);
  $('.blog-posts').append('<div class="well"<h2>' + newEntry.title + '</h2><p>' + newEntry.content + '</p></div>');
 
 
