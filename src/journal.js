be3bcacbc28410e36353565d10b3213f2d929eb5
Christopher Cahill and Devin Sweeting <christopher@>
2019-01-07 10:53:18 -0800

    return number of vowels in content

--- src/journal.js
+++ src/journal.js
@@ -1,11 +1,24 @@
-export function Entry(title, content) {
-    this.title = title,
-    this.content = content;
-  }
+export function Entry(title, content, date) {
+  this.title = title,
+  this.content = content;
+  this.date = date;
+}
 
-  var vowelArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "y", "Y"];
-  var consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Z", "q", "Q"];
+var vowelArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "y", "Y"];
+var consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Z", "q", "Q"];
 
+Entry.prototype.vowelCount = function() {
+  var counter = 0;
+  var array = this.content.split("");
+  console.log(array)
+    for (var i = 0; i <= array.length; i++) {
+      if (vowelArray.includes(array[i]))
+      {
+      counter ++
+      }
+    }
+  return counter;
+}
 
 export function Journal() {
   this.entries = [];
@@ -15,9 +28,9 @@ export function Journal() {
 Journal.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
-}
+};
 
 Journal.prototype.addEntry = function(entry) {
   entry.id = this.assignId();
   this.entries.push(entry);
-}
+};
--- src/main.js
+++ src/main.js
@@ -5,31 +5,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 var journal = new Journal();
-var entryTest = new Entry("title","body");
+var entryTest = new Entry("title","content","date");
 journal.addEntry(entryTest);
-
-
-// $(document).ready(function() {
-//     event.preventDefault();
-//   const title = $('#blogTitleInput').val();
-//   const content = $('#blogContentInput').val();
-//   var entry = new Entry(title, content);
-//   debugger
-//
-//   });
-// });
+$('.blog-posts').append('<div class="well"><h2>' + entryTest.title + entryTest.date + '</h2><p>' + entryTest.content + '</p></div>');
 
 $('#postButton').click(function() {
- const title = $('#blogTitleInput').val();
- const content = $('#blogContentInput').val();
- let newEntry = new Entry(title, content);
- journal.addEntry(newEntry);
- console.log(journal.entries);
- $('.blog-posts').append('<div class="well"<h2>' + newEntry.title + '</h2><p>' + newEntry.content + '</p></div>');
+  const title = $('#blogTitleInput').val();
+  const content = $('#blogContentInput').val();
+  let today = new Date;
+  console.log(today);
+  let yyyy = today.getFullYear();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let date = (dd + "/" + mm + "/" + yyyy);
+  console.log(date);
+  let newEntry = new Entry(title, content, date);
+  journal.addEntry(newEntry);
+  console.log(journal.entries);
+  $('.blog-posts').append('<div class="card container"><h2>' + newEntry.title + newEntry.date + '</h2><p>' + newEntry.content + '</p></div>');
 
 
   $('#flashMessage')
-  .slideDown(500)
-  .delay(3000)
-  .slideUp(500);
+    .slideDown(500)
+    .delay(3000)
+    .slideUp(500);
 });
